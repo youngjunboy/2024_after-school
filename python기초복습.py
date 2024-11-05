f6384f730b5a9fb85174bb8c370f9c0c555899b6
youngboy <121610435+youngjunboy@users.noreply.github.com>
2024-11-05 19:28:30 +0900

    Add files via upload

---
+++ python기초복습.py
@@ -0,0 +1,58 @@
+# print('hello world!')
+
+# print(10//3)
+# print(10/3)
+
+# print("1 \n2")
+# print("'굿'")
+# print('\"굿\"')
+# print('abc Apple\rgood')
+
+# print('abcdefg Apple\rGood')
+
+# a = "hello"
+# print(a * 5)
+# b = "world"
+# print(b[-4])
+
+# a = '%d원 환전' %100000
+# print(a)
+# b = '%s님 마일리지 %d포인트 적립' %('홍영준',5000)
+# print(b)
+
+# print('my name is {}'.format('홍영준'))
+
+# print('my {1} is {0}'.format('홍영준','name'))
+
+# name = '홍영준'
+# age = 19
+# print(f'my name is {name}, age {age}.')# hobby = 'boxing'
+# print(f'my hobby is {hobby}')
+
+# a = 'a:b:c:d:e'
+# print(a.split(':'))
+
+# a = [10, 20, '이름', ['홍길동', '이동수', '김철수'], '프로젝트']
+# print(a[3][1])
+# b = '   홍영준   '
+# print(b.strip())   # strip = 앞 뒤 공백을 빼고 출력
+
+# a1 = 10
+# a2 = 20
+# a = [10, 20]
+# print(a.index(20),'값은',a[1],'이 들어 있음')
+
+# a = [10, 20]
+# a.append(30)
+# a.insert(1, 15)
+# print(a)
+# print(a.count(20))
+# a.sort()
+# print(a)
+
+# a.clear()
+# print(a)
+
+# del(a) a변수 삭제
+# print(a)
+
